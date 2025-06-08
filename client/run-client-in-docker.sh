2c4ebffd1d018fe4699cc838ae4f4a76cd6ca156
zhonghui.wu <1598192613@qq.com>
2025-06-08 20:03:57 +0800

    update

--- client/run-client-in-docker.sh
+++ client/run-client-in-docker.sh
@@ -1,3 +1,12 @@
 #/bin/bash
 
-docker run -d --name=frpc --restart=always -v /data/frpc/frpc.toml:/frp/frpc.toml stilleshan/frpc
+mkdir -p /data/frpc
+cp -f ./frpc.toml  /data/frpc
+docker rmi -f my-frpc-docker-image:0.1
+docker build -t my-frpc-docker-image:0.1 -f ./Dockerfile .
+
+
+docker run -d --name=frpc \
+    --restart=always \
+    -v /data/frpc/frpc.toml:/frp/frpc.toml \
+    my-frpc-docker-image:0.1
--- server/run-server-in-docker.sh
+++ server/run-server-in-docker.sh
@@ -2,11 +2,12 @@
 
 mkdir -p /data/frps
 cp -f ./frps.toml  /data/frps
-docker rmi -f my-frps-docker-imagem:0.1
+docker rmi -f my-frps-docker-image:0.1
 docker build -t my-frps-docker-image:0.1 -f ./Dockerfile .
 
 
-docker run -d --name=frps --restart=always \
+docker run -d --name=frps \
+    --restart=always \
     --network host \
-    -v /data/frps/frps.toml:/frp/frps.toml  \
+    -v /data/frps/frps.toml:/frp/frps.toml \
     my-frps-docker-image:0.1
